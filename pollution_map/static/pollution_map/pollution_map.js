54a442ff2fef36cb866b7f407f5ee1f28f833e1a
Yueh-Han <yuehhanc@andrew.cmu.edu>
2018-07-18 17:21:52 -0400

    Add mobile version

--- pollution_map/static/pollution_map/pollution_map.js
+++ pollution_map/static/pollution_map/pollution_map.js
@@ -6,9 +6,16 @@ var PM025 = [];
 var prev_degO3 = 90;
 var prev_degPM025 = 90;
 var flag = 0; // 0: PM2.5, 1: O3
+var flagMobile = 0;
+
+var mobileScreenSize = 768;
 var gridVals = new Array(84);
 for (var i = 0; i < 84; i++) {
   gridVals[i] = new Array(100);
+}
+var gridValsMobile = new Array(76);
+for (var i = 0; i < 76; i++) {
+  gridValsMobile[i] = new Array(100);
 }
 
 $(document).ready(function() {
@@ -18,7 +25,12 @@ $(document).ready(function() {
         dataType: "text",
         success: function(data) {
             processData(data);
-            addGrids(flag);
+            if (screen.width < mobileScreenSize) {
+                addGridsMobile(flagMobile);
+            } else {
+                addGrids(flag);
+            }
+            
         }
      });
 });
@@ -40,6 +52,10 @@ function processData(allText) {
 }
 
 function o3RadioChecked() {
+    if (screen.width < mobileScreenSize) {
+        o3RadioCheckedMobile();
+        return;
+    }
     var isPM25Arrow = document.getElementById("pm25_arrow");
     if (isPM25Arrow != null) {
         document.getElementById("arrow").innerHTML = "";
@@ -54,7 +70,26 @@ function o3RadioChecked() {
     addGrids(flag);
 }
 
+function o3RadioCheckedMobile() {
+    var isPM25Arrow = document.getElementById("pm25_arrow_mobile");
+    if (isPM25Arrow != null) {
+        document.getElementById("arrow_mobile").innerHTML = "";
+    }
+    var isO3Arrow = document.getElementById("o3_arrow_mobile");
+    if (isO3Arrow == null) {
+        var newArrow = document.createElement("div");
+        newArrow.innerHTML = '<img src="' + "/static/pollution_map/images/blue_arrow.png" + '" class="blue_arrow" id="o3_arrow_mobile">';
+        document.getElementById("arrow_mobile").appendChild(newArrow);
+    }
+    flagMobile = 1;
+    addGridsMobile(flagMobile);
+}
+
 function pm25RadioChecked() {
+    if (screen.width < mobileScreenSize) {
+        o3RadioCheckedMobile();
+        return;
+    }
     var isO3Arrow = document.getElementById("o3_arrow");
     if (isO3Arrow != null) {
         document.getElementById("arrow").innerHTML = "";
@@ -67,6 +102,21 @@ function pm25RadioChecked() {
     }
     flag = 0;
     addGrids(flag);
+}
+
+function pm25RadioCheckedMobile() {
+    var isPM25Arrow = document.getElementById("o3_arrow_mobile");
+    if (isPM25Arrow != null) {
+        document.getElementById("arrow_mobile").innerHTML = "";
+    }
+    var isO3Arrow = document.getElementById("pm25_arrow_mobile");
+    if (isO3Arrow == null) {
+        var newArrow = document.createElement("div");
+        newArrow.innerHTML = '<img src="' + "/static/pollution_map/images/red_arrow.png" + '" class="red_arrow" id="pm25_arrow_mobile">';
+        document.getElementById("arrow_mobile").appendChild(newArrow);
+    }
+    flagMobile = 0;
+    addGridsMobile(flagMobile);
 }
 
 // function getCursor(event) {
@@ -134,8 +184,16 @@ function pm25RadioChecked() {
 //     prev_degPM025 = degPM025;
 //     prev_degO3 = degO3;
 // }
+
 var index4Avg = 0;
 function addGrids(flag) {
+    var screenFactor = 1;
+    var startHeight = 16;
+    // if (screen.width < mobileScreenSize) {
+        // screenFactor = 3.0/4.0;
+        // startHeight = 31;
+        // addGridsMobile(flag);
+    // }
     //gcd(1440, 675) = 45
     var map = document.getElementById("map");
     var map_area = document.getElementById("map_area");
@@ -144,7 +202,7 @@ function addGrids(flag) {
         for (var j = 0; j < 84; j += 6) {
             var newGrid = document.createElement("div");
             var x = i;
-            var y = (16 + j);
+            var y = (startHeight + j);
             var avg = assignAvg(x, y, 0, flag);
             gridVals[j][i] = avg;
             // console.log(j + "," + i + "GridVal: " + gridVals[j][i]);
@@ -195,7 +253,7 @@ function addGrids(flag) {
             var trans = 0.1*multi;
             newGrid.className = "layer";
             newGrid.style.left = x.toString() + "vw";
-            newGrid.style.top = y.toString() + "vh";
+            newGrid.style.top = (y*screenFactor).toString() + "vh";
             if (flag == 0) {
                 newGrid.style.background = "rgba(255,0,0," + trans.toString() +")";
             } else {
@@ -206,6 +264,82 @@ function addGrids(flag) {
         }
     }
     index4Avg = 0;
+}
+
+var index4AvgMobile = 0;
+function addGridsMobile(flag) {
+    var screenFactor = 1;
+    var startHeight = 24;
+    var map = document.getElementById("map_mobile");
+    var map_area = document.getElementById("map_area_mobile");
+    map_area.innerHTML = '<img src="/static/pollution_map/images/pit_map_mobile.png" onclick="searchPointMobile(event)" class="map mobile" id="map_mobile">';
+    for (var i = 0; i < 100; i += 4) {
+        for (var j = 0; j < 76; j += 4) {
+            var newGrid = document.createElement("div");
+            var x = i;
+            var y = (startHeight + j);
+            var avg = assignAvgMobile(x, y, 0, flag);
+            gridValsMobile[j][i] = avg;
+            // console.log(j + "," + i + "GridVal: " + gridVals[j][i]);
+            // console.log("avg: " + avg);
+            // console.log("flag: " + flag);
+            var multi = 1;
+            var meanO3 = 25.8896573;
+            var stdO3 = 12.2525487;
+            var meanPM25 = 9.14702301;
+            var stdPM25 = 5.67227436;
+            if (flag == 0) {
+                if (avg < (meanPM25-stdPM25)) {
+                    multi = 1;
+                } else if (avg < (meanPM25-0.5*stdPM25)) {
+                    multi = 2.5;
+                } else if (avg < (meanPM25-0.25*stdPM25)) {
+                    multi = 3;
+                } else if (avg < meanPM25) {
+                    multi = 3.5;
+                } else if (avg < (meanPM25+0.25*stdPM25)) {
+                    multi = 4;
+                } else if (avg < (meanPM25+0.5*stdPM25)) {
+                    multi = 4.5;
+                } else if (avg < (meanPM25+stdPM25)) {
+                    multi = 5;
+                } else {
+                    multi = 6;
+                }
+            } else {
+                if (avg < (meanO3-stdO3)) {
+                    multi = 1;
+                } else if (avg < (meanO3-0.5*stdO3)) {
+                    multi = 2.5;
+                } else if (avg < (meanO3-0.25*stdO3)) {
+                    multi = 3;
+                } else if (avg < meanO3) {
+                    multi = 3.5;
+                } else if (avg < (meanO3+0.25*stdO3)) {
+                    multi = 4;
+                } else if (avg < (meanO3+0.5*stdO3)) {
+                    multi = 4.5;
+                } else if (avg < (meanO3+stdO3)) {
+                    multi = 5;
+                } else {
+                    multi = 6;
+                }
+            }
+
+            var trans = 0.1*multi;
+            newGrid.className = "layer";
+            newGrid.style.left = x.toString() + "vw";
+            newGrid.style.top = (y*screenFactor).toString() + "vh";
+            if (flag == 0) {
+                newGrid.style.background = "rgba(255,0,0," + trans.toString() +")";
+            } else {
+                newGrid.style.background = "rgba(0,0,255," + trans.toString() +")";
+            }
+            newGrid.onclick = searchPointMobile;
+            map_area.appendChild(newGrid);
+        }
+    }
+    index4AvgMobile = 0;
 }
 
 function assignAvg(x, y, index, flag) {
@@ -260,12 +394,73 @@ function assignAvg(x, y, index, flag) {
     }
 }
 
+function assignAvgMobile(x, y, index, flag) {
+    var map = document.getElementById("map_mobile");
+    var sum = 0;
+    var count = 0;
+    var scaleX = map.clientWidth/100.0;
+    var scaleY = (map.clientHeight/0.76)/100.0;
+    var xMin = toLonMobile(x*scaleX, map);
+    var yMax = toLatMobile(y*scaleY, map);
+    var xMax = toLonMobile((x+2)*scaleX, map);
+    var yMin = toLatMobile((y+2)*scaleY, map);
+    // console.log("gridX: " + xMin + " gridY:" + yMin);
+    // console.log("gridXM: " + xMax + " gridYM:" + yMax);
+    for (i = index; i < latitude.length; i++) {
+        if (latitude[i] < yMax && latitude[i] >= yMin && longitude[i] < xMax && longitude[i] >= xMin) {
+            if (flag == 0) {
+                sum += PM025[i];
+            } else {
+                sum += O3[i];
+            }
+            count += 1;
+        }
+        if (longitude[i] >= xMax) {
+            index4AvgMobile = i;
+            break;
+        }
+    }
+    // console.log("count: " + count + " sum: " + sum);
+    if (count > 0) {
+        // console.log("count: " + count);
+        return sum/count;
+    } else {
+        var i = x;
+        var j = y-24;
+        if (j-4 > 0) {
+            sum += gridValsMobile[j-4][i];
+            count++;
+        }
+        if (i-4 > 0) {
+            sum += gridValsMobile[j][i-4];
+            count++;
+        }
+        
+        if (sum > 0) {
+            return sum/count;
+        } else {
+            // console.log("Client Width: " + x*scaleX + ", Client Height:" + y*scaleY);
+            // console.log("Long: " + xMin + ", Lat:" + yMin);
+            // console.log("Long: " + xMax+ ", Lat:" + yMax);
+            return 1;
+        }
+    }
+}
+
 function toLat(y, map) {
     return 40.48 - (0.1/(1+map.clientHeight))*(y-129);
 }
 
+function toLatMobile(y, map) {
+    return 40.48 - (0.18/(1+map.clientHeight))*(y-425);
+}
+
 function toLon(x, map) {
     return -80.12 + (0.29/(1+map.clientWidth))*x;
+}
+
+function toLonMobile(x, map) {
+    return -80.08 + (0.21/(1+map.clientWidth))*x;
 }
 
 function searchPoint(event) {
@@ -345,6 +540,97 @@ function searchPoint(event) {
     prev_degO3 = degO3;
 
     var map_area = document.getElementById("map_area");
+    if (document.getElementById("marker") != null) {
+        document.getElementById("marker").remove();
+    }
+    // map_area.innerHTML = map_area.innerHTML + '<div onclick="searchPoint(event)" style="z-index: 5; border-color: orange; border-style: solid; border-width: 6px; width: 1.5vw; height: 1.5vw; z-index: 5; position: absolute; left: ' + 
+    //                       (x-8) + 'px; top: ' + (y-8) + 'px;" id="marker"></div>';
+    var newCursor = document.createElement("div");
+    newCursor.className = "cursor";
+    newCursor.id = "marker";
+    newCursor.style.left = (x-8).toString() + "px";
+    newCursor.style.top = (y-8).toString() + "px";
+    newCursor.onclick = searchPoint;
+    map_area.appendChild(newCursor);
+}
+
+function searchPointMobile(event) {
+    var x = event.clientX;
+    var y = event.clientY;
+    var map = document.getElementById("map_mobile");
+    console.log("x: " + x + ", y:" + y);
+    // 1440 800
+    // coord: top-left:40.48, -80.08 bottom-right: 40.30, -79.87
+    var lat = 40.48 - (0.18/(1+map.clientHeight))*(y-425);//+1 to avoid zero division
+    var lon = -80.08 + (0.21/(1+map.clientWidth))*x;
+    console.log("Latitude: " + lat + ", Longitude:" + lon);
+    var dist = 2147483647;
+    var index = 0;
+    for (i = 0; i < latitude.length; i++) {
+        var dX = latitude[i]-lat;
+        var dY = longitude[i]-lon;
+        var curr_dist = dX*dX + dY*dY;
+        if (curr_dist < dist) {
+            dist = curr_dist;
+            index = i;
+        }
+    }
+    // console.log("Client Width: " + map.clientWidth + ", Client Height:" + map.clientHeight);
+    // console.log("CO: " + CO[index]);
+    // console.log("O3: " + O3[index]); //max: 38, min:22, avg: 30, mean: 25.8896573, std=12.2525487
+    // console.log("PM2.5: " + PM025[index]); //max:14, min:2, avg: 8, mean: 9.14702301, std=5.67227436
+    var meanO3 = 25.8896573;
+    var stdO3 = 12.2525487;
+    var meanPM25 = 9.14702301;
+    var stdPM25 = 5.67227436;
+
+    var degO3 = (O3[index] - (meanO3 - 2*stdO3))*180/(4*stdO3);
+    var degPM025 =  (PM025[index] - (meanPM25 - 2*stdPM25))*180/(4*stdPM25);
+    if (degO3 > 180) {
+        degO3 = 180;
+    }
+    if (degO3 < 0) {
+        degO3 = 0;
+    }
+    if (degPM025 > 180) {
+        degPM025 = 180;
+    }
+    if (degPM025 < 0) {
+        degPM025 = 0;
+    }
+    // console.log("PM25: " + degPM025 + ", " + PM025[index]);
+    // console.log("O3: " + degO3 + ", " + O3[index]);
+    
+    // aminate rotation
+    $({deg: prev_degO3}).animate({deg: degO3}, {
+        duration: 200,
+        step: function(now) {
+            $("#o3_arrow_mobile").css({
+                transform: 'rotate(' + (now - 90) + 'deg)'
+            });
+        }
+    });
+    $({deg: prev_degPM025}).animate({deg: degPM025}, {
+        duration: 200,
+        step: function(now) {
+            // in the step-callback (that is fired each step of the animation),
+            // you can use the `now` paramter which contains the current
+            // animation-position (`0` up to `angle`)
+            $("#pm25_arrow_mobile").css({
+                transform: 'rotate(' + (now - 90) + 'deg)'
+            });
+        }
+    });
+
+    // console.log("Rotate O3: " + rotateO3);
+    // console.log("Rotate PM25: " + rotatePM025);
+    // console.log("O3 deg: " + degO3);
+    // console.log("PM2.5 deg: " + degPM025);
+    ////////////////
+    prev_degPM025 = degPM025;
+    prev_degO3 = degO3;
+
+    var map_area = document.getElementById("map_area_mobile");
     if (document.getElementById("marker") != null) {
         document.getElementById("marker").remove();
     }
