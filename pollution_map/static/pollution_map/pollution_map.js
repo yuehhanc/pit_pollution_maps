be37dfd31e4419aa14912de67d18f613b713d5cd
Yueh-Han <yuehhanc@andrew.cmu.edu>
2018-07-19 00:47:57 -0400

    add clairton/lincoln locations into the map

--- pollution_map/static/pollution_map/pollution_map.js
+++ pollution_map/static/pollution_map/pollution_map.js
@@ -7,6 +7,7 @@ var prev_degO3 = 90;
 var prev_degPM025 = 90;
 var flag = 0; // 0: PM2.5, 1: O3
 var flagMobile = 0;
+var isClairton = 0;
 
 var mobileScreenSize = 768;
 var gridVals = new Array(84);
@@ -119,85 +120,19 @@ function pm25RadioCheckedMobile() {
     addGridsMobile(flagMobile);
 }
 
-// function getCursor(event) {
-//     var x = event.clientX;
-//     var y = event.clientY;
-//     var lat = 40.520537 - 0.00025509*(x-129);
-//     var lon = -80.229309 + 0.00032544*(y-1440);
-//     $.ajax({
-//         url: "/pollution_map/get_cursor_json",
-//         data: {'lat': lat, 'lon': lon},
-//         datatype: "json",
-//         success: rotateDials,
-//     });
-// }
-
-// function rotateDials(coord) {
-//     var lat = coord[0];
-//     var lon = coord[1];
-
-//     var dist = 2147483647;
-//     var index = 0;
-//     for (i = 0; i < latitude.length; i++) {
-//         var dX = latitude[i]-lat;
-//         var dY = longitude[i]-lon;
-//         var curr_dist = dX*dX + dY*dY;
-//         if (curr_dist < dist) {
-//             dist = curr_dist;
-//             index = i;
-//         }
-//     }
-
-//     var degO3 = (O3[index] - 22)*180/16.0;
-//     var degPM025 =  (PM025[index] - 2)*180/10.0;
-//     if (degO3 > 180) {
-//         degO3 = 180;
-//     }
-//     if (degO3 < 0) {
-//         degO3 = 0;
-//     }
-//     if (degPM025 > 180) {
-//         degPM025 = 180;
-//     }
-//     if (degPM025 < 0) {
-//         degPM025 = 0;
-//     }
-    
-//     // aminate rotation
-//     $({deg: prev_degO3}).animate({deg: degO3}, {
-//         duration: 200,
-//         step: function(now) {
-//             $("#o3_arrow").css({
-//                 transform: 'rotate(' + (now - 90) + 'deg)'
-//             });
-//         }
-//     });
-//     $({deg: prev_degPM025}).animate({deg: degPM025}, {
-//         duration: 200,
-//         step: function(now) {
-//             $("#pm25_arrow").css({
-//                 transform: 'rotate(' + (now - 90) + 'deg)'
-//             });
-//         }
-//     });
-//     ////////////////
-//     prev_degPM025 = degPM025;
-//     prev_degO3 = degO3;
-// }
-
 var index4Avg = 0;
 function addGrids(flag) {
     var screenFactor = 1;
     var startHeight = 16;
-    // if (screen.width < mobileScreenSize) {
-        // screenFactor = 3.0/4.0;
-        // startHeight = 31;
-        // addGridsMobile(flag);
-    // }
+
     //gcd(1440, 675) = 45
     var map = document.getElementById("map");
     var map_area = document.getElementById("map_area");
-    map_area.innerHTML = '<img src="/static/pollution_map/images/pit_map.png" onclick="searchPoint(event)" class="map" id="map">';
+    if (isClairton == 1) {
+        map_area.innerHTML = '<img src="/static/pollution_map/images/pit_map_clairton.png" onclick="searchPoint(event)" class="map" id="map">';
+    } else {
+        map_area.innerHTML = '<img src="/static/pollution_map/images/pit_map.png" onclick="searchPoint(event)" class="map" id="map">';
+    }
     for (var i = 0; i < 100; i += 4) {
         for (var j = 0; j < 84; j += 6) {
             var newGrid = document.createElement("div");
@@ -448,7 +383,12 @@ function assignAvgMobile(x, y, index, flag) {
 }
 
 function toLat(y, map) {
-    return 40.48 - (0.1/(1+map.clientHeight))*(y-129);
+    if (isClairton == 0) {
+        return 40.48 - (0.1/(1+map.clientHeight))*(y-129);
+    } else {
+        return 40.38 - (0.11/(1+map.clientHeight))*(y-129);
+    }
+    
 }
 
 function toLatMobile(y, map) {
@@ -456,7 +396,11 @@ function toLatMobile(y, map) {
 }
 
 function toLon(x, map) {
-    return -80.12 + (0.29/(1+map.clientWidth))*x;
+    if (isClairton == 0) {
+        return -80.12 + (0.29/(1+map.clientWidth))*x;
+    } else {
+        return -80.11 + (0.26/(1+map.clientWidth))*x;
+    }
 }
 
 function toLonMobile(x, map) {
@@ -470,8 +414,15 @@ function searchPoint(event) {
     // console.log("x: " + x + ", y:" + y);
     // 1440 800
     // coord: top-left:40.38, -80.12 bottom-right: 40.48, -79.83
-    var lat = 40.48 - (0.1/(1+map.clientHeight))*(y-129);//+1 to avoid zero division
-    var lon = -80.12 + (0.29/(1+map.clientWidth))*x;
+    var lat, lon;
+    if (isClairton == 0) {
+        lat = 40.48 - (0.1/(1+map.clientHeight))*(y-129);//+1 to avoid zero division
+        lon = -80.12 + (0.29/(1+map.clientWidth))*x;
+    } else {
+        lat = 40.38 - (0.11/(1+map.clientHeight))*(y-129);//+1 to avoid zero division
+        lon = -80.11 + (0.26/(1+map.clientWidth))*x; 
+    }
+    
     console.log("Latitude: " + lat + ", Longitude:" + lon);
     var dist = 2147483647;
     var index = 0;
@@ -660,6 +611,24 @@ function detectMobile() {
     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
         window.location = "http://www.youtube.com";
     }
+}
+
+function goToClairton() {
+    isClairton = 1;
+    var map_area = document.getElementById("map_area");
+    map_area.innerHTML = '<img src="/static/pollution_map/images/pit_map_clairton.png" onclick="searchPoint(event)" class="map" id="map">';
+    addGrids(flag);
+    $("#down_arrow").hide();
+    $("#up_arrow").show();
+}
+
+function goToPit() {
+    isClairton = 0;
+    var map_area = document.getElementById("map_area");
+    map_area.innerHTML = '<img src="/static/pollution_map/images/pit_map.png" onclick="searchPoint(event)" class="map" id="map">';
+    addGrids(flag);
+    $("#up_arrow").hide();
+    $("#down_arrow").show();
 }
 // window.location.reload(true);
 // window.onload = function() {
