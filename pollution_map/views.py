12166712b0034a090ecf5da5464696bd369e1aa2
Yueh-Han <yuehhanc@andrew.cmu.edu>
2018-11-27 18:00:58 -0500

    Update views.py

--- pollution_map/views.py
+++ pollution_map/views.py
@@ -7,7 +7,7 @@ from pollution_map.forms import FileForm, EntryForm
 from pollution_map.models import File, Entry
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404, redirect
-from django.core.urlresolvers import reverse
+# from django.core.urlresolvers import reverse
 import os, shutil, sys
 
 ### Third Adjustment
