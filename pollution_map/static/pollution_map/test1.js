6c2454ec9deae9c8d46b78fee697712845460e04
Yueh-Han <yuehhanc@andrew.cmu.edu>
2019-03-21 21:42:13 -0400

    matching colors

--- pollution_map/static/pollution_map/test1.js
+++ pollution_map/static/pollution_map/test1.js
@@ -186,7 +186,7 @@ function addGrids(flag) {
                 } else if (avg < meanO3) {
                     multi = 1.5;
                 } else if (avg < (meanO3+0.25*stdO3)) {
-                    multi = 3.5;
+                    multi = 2.5;
                 } else if (avg < (meanO3+0.5*stdO3)) {
                     multi = 3.5;
                 } else if (avg < (meanO3+stdO3)) {
