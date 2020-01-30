fde9bcb80edfa2fd700b040fde849b79af5f701a
rhendre <rhendre@andrew.cmu.edu>
2020-01-30 13:33:36 -0500

    Update settings.py

--- webapps/settings.py
+++ webapps/settings.py
@@ -20,13 +20,14 @@ import os
 # for relative imports by default.
 
 # Celery settings
-CELERY_BROKER_URL = 'redis://localhost:6379'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_BROKER_URL = 'redis://127.0.0.1:6379'
+CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_TIMEZONE = 'UTC'
 CELERY_ENABLE_UTC = True
+CELERY_ALWAYS_EAGER = True
 CELERY_BEAT_SCHEDULE = {
     'task-number-one': {
         'task': 'task_number_one',
