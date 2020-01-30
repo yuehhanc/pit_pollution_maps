d70c7459d734bb288858eaa7453877189cf8beca
anguloa <46971621+anguloa@users.noreply.github.com>
2020-01-30 14:36:01 -0500

    Updating redis server and celery connection

--- webapps/settings.py
+++ webapps/settings.py
@@ -20,8 +20,8 @@ import os
 # for relative imports by default.
 
 # Celery settings
-CELERY_BROKER_URL = 'redis://127.0.0.1:6379'
-CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379'
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TASK_SERIALIZER = 'json'
