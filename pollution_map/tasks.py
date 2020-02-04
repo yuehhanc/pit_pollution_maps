4e667c36caee33e5cdf1d7bc8df99b75475617ae
anguloa <46971621+anguloa@users.noreply.github.com>
2020-02-03 21:18:19 -0500

    Reverting update tasks.py

--- pollution_map/tasks.py
+++ pollution_map/tasks.py
@@ -6,11 +6,11 @@ import shutil
 from celery import task
 
 
-@task()
+@task(name='task_number_one')
 def task_number_one():
     print("It works")
     currFileName = "Interpolated_Map"
-   
+    try:
         # AWS
         for file in os.listdir("/home/ubuntu/pit_pollution_maps/pollution_map/static/pollution_map/data/"):
             fileName = file.split("/")[-1]
@@ -18,3 +18,11 @@ def task_number_one():
                 currFileName = fileName
         print(currFileName)
         shutil.copyfile("/home/ubuntu/pit_pollution_maps/pollution_map/static/pollution_map/data/" + currFileName, "/home/ubuntu/pit_pollution_maps/pollution_map/static/pollution_map/Interpolated_Map.csv")
+    except:
+        # Local
+        for file in os.listdir("pollution_map/static/pollution_map/data/"):
+            fileName = file.split("/")[-1]
+            if fileName[:-4] > currFileName[:-4]:
+                currFileName = fileName
+        print(currFileName)
+        shutil.copyfile("pollution_map/static/pollution_map/data/" + currFileName, "pollution_map/static/pollution_map/Interpolated_Map.csv")
